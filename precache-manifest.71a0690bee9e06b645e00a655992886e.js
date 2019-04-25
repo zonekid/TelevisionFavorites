32a5d45c8cc14f988c1b0d116fa4438bf233d76a
zonekid <marcusmiller220@gmail.com>
2019-04-25 15:15:59 -0400

    Updates

---
+++ precache-manifest.71a0690bee9e06b645e00a655992886e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d0b9b9e4101ed41f015009ae714b36",
+    "url": "/FavoriteShows/static/media/Atlantabackdrop.f2d0b9b9.jpg"
+  },
+  {
+    "revision": "47c4eb7082fe6e638741",
+    "url": "/FavoriteShows/static/css/main.02e25025.chunk.css"
+  },
+  {
+    "revision": "41d0f191f17204567de0",
+    "url": "/FavoriteShows/static/js/runtime~main.d762b86f.js"
+  },
+  {
+    "revision": "f74443392ea48344265b",
+    "url": "/FavoriteShows/static/js/2.a78d5128.chunk.js"
+  },
+  {
+    "revision": "bb59be9bee3e42e2f5bb9338f1f01524",
+    "url": "/FavoriteShows/static/media/rugrattv.bb59be9b.png"
+  },
+  {
+    "revision": "c178573f8f6ef057846ca8653ba89f7b",
+    "url": "/FavoriteShows/static/media/backdroptwin.c178573f.jpg"
+  },
+  {
+    "revision": "47c4eb7082fe6e638741",
+    "url": "/FavoriteShows/static/js/main.9ced9eb5.chunk.js"
+  },
+  {
+    "revision": "6283c46913f9c94c7865c3e0757f4da3",
+    "url": "/FavoriteShows/static/media/lucyback.6283c469.png"
+  },
+  {
+    "revision": "b11655dfe0b779320dbf544ca5c4f89b",
+    "url": "/FavoriteShows/static/media/twlightback2.b11655df.jpg"
+  },
+  {
+    "revision": "62a36e85d2355b9263a99d8a9f13abb1",
+    "url": "/FavoriteShows/static/media/wireback.62a36e85.jpg"
+  },
+  {
+    "revision": "ab11510a50e14ee40152297cbdbb1aa5",
+    "url": "/FavoriteShows/static/media/goldengirlsback.ab11510a.jpg"
+  },
+  {
+    "revision": "bab375a268df634d155102c886bc7ad5",
+    "url": "/FavoriteShows/static/media/gotbackdrop.bab375a2.jpg"
+  },
+  {
+    "revision": "4071824a573256ff2da18df763350c7d",
+    "url": "/FavoriteShows/index.html"
+  }
+];
