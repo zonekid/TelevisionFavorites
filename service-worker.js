f2d42dd1c8c65ea60b54ab63183ec3c13607cf84
zonekid <marcusmiller220@gmail.com>
2019-04-25 15:51:05 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TelevisionFavorites/components/home/precache-manifest.64d1efb69f17f5b432993ac58ae1eae9.js"
+  "/TelevisionFavorites/precache-manifest.529a0fa4461db8c422ff0adebab68847.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TelevisionFavorites/components/home/index.html", {
+workbox.routing.registerNavigationRoute("/TelevisionFavorites/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
