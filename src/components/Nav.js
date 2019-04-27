63333ac67bedf8664d79ad1e63889ed44b94c59d
zonekid <marcusmiller220@gmail.com>
2019-04-27 01:10:12 -0400

    back to beg9

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -36,16 +36,16 @@ const NavBar = () => {
 
     <Nav className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/lucy">I Love Lucy</Nav.Link>
-      <Nav.Link href="/TelevisionFavorites/#/twin-peaks">Twin Peaks</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/twilight">The Twilight Zone</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/atlanta">Atlanta</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/golden">The Golden Girls</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/thrones">Game of Thrones</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/wire">The Wire</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/sopranos">The Sopranos</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/martin">Martin</Nav.Link>
-      <Nav.Link href="TelevisionFavorites/#/prince">The Fresh Prince</Nav.Link>
+      <Nav.Link href="/lucy">I Love Lucy</Nav.Link>
+      <Nav.Link href="/twin-peaks">Twin Peaks</Nav.Link>
+      <Nav.Link href="/twilight">The Twilight Zone</Nav.Link>
+      <Nav.Link href="/atlanta">Atlanta</Nav.Link>
+      <Nav.Link href="/golden">The Golden Girls</Nav.Link>
+      <Nav.Link href="/thrones">Game of Thrones</Nav.Link>
+      <Nav.Link href="/wire">The Wire</Nav.Link>
+      <Nav.Link href="/sopranos">The Sopranos</Nav.Link>
+      <Nav.Link href="/martin">Martin</Nav.Link>
+      <Nav.Link href="/prince">The Fresh Prince</Nav.Link>
     </Nav>TelevisionFavorites/#/
     TelevisionFavorites/#/
   </Navbar.Collapse>
